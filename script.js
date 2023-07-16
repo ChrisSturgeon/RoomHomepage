af40b668e6ddca8a1c6bea0673325710024ab651
csturgeon <sturgeon.chris@gmail.com>
2023-07-16 07:38:21 +0100

    Add basic hamburger menu toggle

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('Test');
+
+const hamburger = document.querySelector('#hamburger');
+
+hamburger.addEventListener('click', () => {
+  const navMenu = document.querySelector('#nav-wrapper');
+  navMenu.classList.toggle('hidden');
+});
