5ad004c5965882c3c3daafd654e42fac8123c485
csturgeon <sturgeon.chris@gmail.com>
2023-09-16 07:35:25 +0100

    Add accessible hamburger

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-console.log('Test');
+const hamburgerOpenBtn = document.querySelector('#hamburger-btn-open');
+const hamburgerCloseBtn = document.querySelector('#hamburger-btn-close');
+const slideNav = document.querySelector('#slide-nav');
 
-const hamburger = document.querySelector('#hamburger');
+function openHamburgerMenu() {
+  slideNav.classList.add('visible');
+  slideNav.classList.add('active');
+  setTimeout(() => {
+    hamburgerCloseBtn.focus();
+  }, 1);
+}
 
-hamburger.addEventListener('click', () => {
-  const navMenu = document.querySelector('#nav-wrapper');
-  navMenu.classList.toggle('hidden');
-});
+function closeHamburgerMenu() {
+  slideNav.classList.remove('active');
+  setTimeout(() => {
+    hamburgerOpenBtn.focus();
+  }, 1);
+  setTimeout(() => {
+    slideNav.classList.remove('visible');
+  }, 250);
+}
+
+// Event listeners
+hamburgerOpenBtn.addEventListener('click', openHamburgerMenu);
+hamburgerCloseBtn.addEventListener('click', closeHamburgerMenu);
